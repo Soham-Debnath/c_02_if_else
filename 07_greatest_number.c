192621b262a302ff674550f0da52bd095af95263
Soham-Debnath <20debnathsoham04@gmail.com>
2025-01-08 00:53:17 +0530

    greateast_no

---
+++ 07_greatest_number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if(a>b&&a>c){
+        printf("%d is greatest",a);
+    }
+    if(b>a&&b>c){
+        printf("%d is greatest",b);
+    }
+    if(c>b&&c>a){
+        printf("%d is greatest",c);
+    }
+    return 0;
+}
